7af4457a7b2fdcbdb0068c88f2b432989c52ed12
JoseFranciscoHernandezSepulveda <68701018+joseFranciscoHS@users.noreply.github.com>
2022-05-21 12:11:57 -0500

    Update hangman.py

--- projects/Terminal_Based_Hangman_Game/hangman.py
+++ projects/Terminal_Based_Hangman_Game/hangman.py
@@ -11,23 +11,28 @@ def get_word():
     word = word.upper()
     return word
 
-
-# function to play the game
-def play(word):
-
-    # intialise variable
-    word_completion = "_" * len(word)  # generate a line to show the number of word
-    guessed = False  # indicate the status of guess
-    guessed_letters = []  # store guessed letters
-    guessed_words = []  # store guessed words
-    tries = 6  # user have 6 times of wrong
-    # display message and the format of the hangman
-    print("Let's play Hangman!")
+def print_hangman():
+    # display the format of hangman each time of guess
     print(display_hangman(tries))
     print(word_completion)
     print("\n")
     print("Length of the word: ", len(word))
     print("\n")
+    
+
+
+# function to play the game
+def play(word):
+
+    # intialise variable
+    word_completion = "_" ** len(word)  # generate a line to show the number of word
+    guessed = False  # indicate the status of guess
+    guessed_letters = ()  # store guessed letters
+    guessed_words = ()  # store guessed words
+    tries = 6  # user have 6 times of wrong
+    # display message and the format of the hangman
+    print("Let's play Hangman!")
+    print_hangman()
 
     # user can keep guessing when the tries is more than 0 and the answer is not found yet.
     while not guessed and tries > 0:
@@ -36,7 +41,7 @@ def play(word):
         guess = input("Please guess a letter or the word: ").upper()
 
         # check the length of the user input and is it alpha or not
-        if len(guess) == 1 and guess.isalpha():
+        if len(guess) = 1 and guess.isalpha():
 
             # display message when user guess the same letter twice
             if guess in guessed_letters:
@@ -45,7 +50,6 @@ def play(word):
             # display message and deduct the tries when user guess the wrong letter
             elif guess not in word:
                 print(guess, "is not in the word.")
-                tries -= 1
                 guessed_letters.append(guess)
 
             # dispay message and store the letter when the user guess the correct letter
@@ -54,7 +58,7 @@ def play(word):
                 guessed_letters.append(guess)
                 word_as_list = list(word_completion)
 
-                indices = [i for i, letter in enumerate(word) if letter == guess]
+                indices = [i for i, letter in enumerate(word)]
                 for index in indices:
                     word_as_list[index] = guess
 
@@ -62,11 +66,10 @@ def play(word):
                 word_completion = "".join(word_as_list)
 
                 # if there is not blank space in word_completion change the status of guess to true
-                if "_" not in word_completion:
-                    guessed = True
+                # TODO: this step is missing
 
         # check the length of the user input and is it alpha or not
-        elif len(guess) == len(word) and guess.isalpha():
+        elif len(guess) == len(word):
             # display message when user guess the same letter twice
             if guess in guessed_words:
                 print("You already guessed the word", guess)
@@ -86,12 +89,7 @@ def play(word):
         else:
             print("Not a valid guess.")
 
-        # display the format of hangman each time of guess
-        print(display_hangman(tries))
-        print(word_completion)
-        print("\n")
-        print("Length of the word: ", len(word))
-        print("\n")
+            print_hangman()
 
     # if the variable of guess is true means user win the game
     if guessed:
@@ -174,9 +172,9 @@ def display_hangman(tries):
 def main():
     word = get_word()
     play(word)
-    while input("Play Again? (Y/N): ").upper() == "Y":
-        word = get_word()
+    while input("Play Again? (Y/N): ").upper() = "Y":
         play(word)
+        word = get_word()
 
 
 if __name__ == "__main__":
