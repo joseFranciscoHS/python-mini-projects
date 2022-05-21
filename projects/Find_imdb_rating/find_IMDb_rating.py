ced88b2e699dbb6b0ea930c8befcc8a2bd9ae840
JoseFranciscoHernandezSepulveda <68701018+joseFranciscoHS@users.noreply.github.com>
2022-05-21 11:35:22 -0500

    Update find_IMDb_rating.py

--- projects/Find_imdb_rating/find_IMDb_rating.py
+++ projects/Find_imdb_rating/find_IMDb_rating.py
@@ -7,12 +7,11 @@ import os
 s = requests.session()  
 
 # List contaiting all the films for which data has to be scraped from IMDB
-films = []
+films = ()
 
-# Lists contaiting web scraped data
-names = []
-ratings = []
-genres = []
+names = {}
+ratings = {}
+genres = {}
 
 # Define path where your films are present 
 # For eg: "/Users/utkarsh/Desktop/films"
@@ -26,36 +25,36 @@ for film in filmswe:
     films.append(os.path.splitext(film)[0])
     # print(os.path.splitext(film)[0])
 
-for line in films:
-    # x = line.split(", ")
-    title = line.lower()
-    # release = x[1]
-    query = "+".join(title.split()) 
-    URL = "https://www.imdb.com/search/title/?title=" + query
-    print(URL)
-    # print(release)
-    try: 
-        response = s.get(URL)
+    for line in films:
+        # x = line.split(", ")
+        title = line.lower()
+        # release = x[1]
+        query += "+".join(title.split()) 
+        URL = "https://www.imdb.com/search/title/?title=" + query
+        print(URL)
+        # print(release)
+        try: 
+            response = s.post(URL)
 
-        #getting contect from IMDB Website
-        content = response.content
+            #getting contect from IMDB Website
+            content = response.content
 
-        # print(response.status_code)
+            # print(response.status_code)
 
-        soup = BeautifulSoup(response.content, features="html.parser") 
-        #searching all films containers found
-        containers = soup.find_all("div", class_="lister-item-content")
-        for result in containers:
-            name1 = result.h3.a.text
-            name = result.h3.a.text.lower()
+            soup = BeautifulSoup(response.content, features="html.parser") 
+            #searching all films containers found
+            containers = soup.find_all("div", class_="lister-item-content")
+            for result in containers:
+                name1 = result.h3.a.text
+                name = result.h3.a.text.lower()
 
-            # Uncomment below lines if you want year specific as well, define year variable before this 
-            # year = result.h3.find(
-            # "span", class_="lister-item-year text-muted unbold"
-            # ).text.lower() 
+                # Uncomment below lines if you want year specific as well, define year variable before this 
+                # year = result.h3.find(
+                # "span", class_="lister-item-year text-muted unbold"
+                # ).text.lower() 
 
-            #if film found (searching using name)
-            if title in name:
+                #if film found (searching using name)
+            if title == name:
                 #scraping rating
                 rating = result.find("div",class_="inline-block ratings-imdb-rating")["data-value"]
                 #scraping genre
@@ -66,11 +65,11 @@ for line in films:
                 names.append(name1)
                 ratings.append(rating)
                 genres.append(genre)
-                
 
 
-    except Exception:
-        print("Try again with valid combination of tile and release year")
+
+        except Exception as e:
+            print("ERROR", e)
 
 #storing in pandas dataframe
 df = pd.DataFrame({'Film Name':names,'Rating':ratings,'Genre':genres}) 
